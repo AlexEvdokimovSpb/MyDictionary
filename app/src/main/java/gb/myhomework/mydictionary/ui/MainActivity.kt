92081ce9e7a0ef5194e54078a2fafbbbc078a1e7
AlexEvdokimovSpb <3416216@gmail.com>
2021-06-27 17:22:10 +0300

    lesson one реализация MVP

--- app/src/main/java/gb/myhomework/mydictionary/ui/MainActivity.kt
+++ app/src/main/java/gb/myhomework/mydictionary/ui/MainActivity.kt
@@ -99,8 +99,4 @@ class MainActivity : BaseActivity<AppState>() {
         error_linear_layout.visibility = View.VISIBLE
     }
 
-    companion object {
-        private const val BOTTOM_SHEET_FRAGMENT_DIALOG_TAG =
-            "74a54328-5d62-46bf-ab6b-cbf5fgt0-092395"
-    }
 }
